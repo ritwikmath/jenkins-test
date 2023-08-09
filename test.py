1d4a1c12236d67a068c91e27c6164cf01ff68c54
ritwikmath <ritwikmath@gmail.com>
2023-08-09 14:42:01 +0530

    Test added

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+from app import addition
+
+class FirstUnittest(unittest.TestCase):
+    def test_sum(self):
+        num1 = 10
+        num2 = 20
+        result = addition(num1, num2)
+        self.assertEqual(result, num1 + num2)
+
+if __name__ == "__main__":
+    unittest.main()
