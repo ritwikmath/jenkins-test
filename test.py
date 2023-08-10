2ed7040e86a638028a1fb53b61045d90e1da7365
ritwikmath <ritwikmath@gmail.com>
2023-08-10 08:30:23 +0530

    Updated code

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-from app import addition
+from app import addition, substraction
 
 class FirstUnittest(unittest.TestCase):
     def test_sum(self):
@@ -7,6 +7,12 @@ class FirstUnittest(unittest.TestCase):
         num2 = 20
         result = addition(num1, num2)
         self.assertEqual(result, num1 + num2)
+    
+    def test_minus(self):
+        num1 = 20
+        num2 = 10
+        result = substraction(num1, num2)
+        self.assertEqual(result, num1 - num2)
 
 if __name__ == "__main__":
     unittest.main()
